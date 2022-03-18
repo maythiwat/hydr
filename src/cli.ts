46329fcd690872eee36137ea6ff666a36ef4208c
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2022-03-18 18:57:56 +0700

    refactor: indent, semicolon

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs/yargs'
-import * as hydr from '.'
+import yargs from 'yargs/yargs';
+import * as hydr from '.';
 
 const options = yargs(process.argv.slice(2))
-    .demandCommand(1, 1)
-    .options({
-        chunks: { alias: 'c', type: 'number', describe: 'number of split chunks', default: 4, demandOption: false },
-        delay: { alias: 'd', type: 'number', describe: 'delay per chunks (in ms)', default: 100, demandOption: false },
-        timeout: { alias: 't', type: 'number', describe: 'timeout per chunks download (in ms, 0 = forever)', default: 0, demandOption: false },
-        retry: { alias: 'r', type: 'number', describe: 'max retries on chunks download timeout', demandOption: false },
-        outFlie: { alias: 'o', type: 'string', describe: 'save to filename', demandOption: false },
-    })
-.parseSync() as any
+  .demandCommand(1, 1)
+  .options({
+    chunks: { alias: 'c', type: 'number', describe: 'number of split chunks', default: 4, demandOption: false },
+    delay: { alias: 'd', type: 'number', describe: 'delay per chunks (in ms)', default: 100, demandOption: false },
+    timeout: { alias: 't', type: 'number', describe: 'timeout per chunks download (in ms, 0 = forever)', default: 0, demandOption: false },
+    retry: { alias: 'r', type: 'number', describe: 'max retries on chunks download timeout', demandOption: false },
+    outFlie: { alias: 'o', type: 'string', describe: 'save to filename', demandOption: false }
+  })
+.parseSync() as any;
 
-console.log('')
+console.log('');
 
-;(async () => {
-    await hydr.start(options._[0], options.chunks, options.outFlie, options.delay, options.timeout, options.retry)
-})()
+(async () => {
+  await hydr.start(options._[0], options.chunks, options.outFlie, options.delay, options.timeout, options.retry);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,251 +1,252 @@
-import fs from 'fs/promises'
-import path from 'path'
-import { EventEmitter } from 'events'
+import fs from 'fs/promises';
+import path from 'path';
+import { EventEmitter } from 'events';
 
-import axios from 'axios'
-import mime from 'mime-types'
-import cliProgress from 'cli-progress'
+import axios from 'axios';
+import mime from 'mime-types';
+import cliProgress from 'cli-progress';
 
-axios.defaults.headers['user-agent'] = 'hydr/1.0'
+axios.defaults.headers['user-agent'] = 'hydr/1.0';
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function setUserAgent(userAgent: string) {
-    axios.defaults.headers['user-agent'] = userAgent
+  axios.defaults.headers['user-agent'] = userAgent;
 }
 
 async function fetchChunk(url: string, byteStart: number, byteEnd: number, timeout: number = 0) {
-    return await axios.get(url, {
-        headers: {
-            range: `bytes=${byteStart}-${byteEnd}`
-        },
-        responseType: 'stream',
-        timeout
-    })
+  return await axios.get(url, {
+    headers: {
+      range: `bytes=${byteStart}-${byteEnd}`
+    },
+    responseType: 'stream',
+    timeout
+  });
 }
 
 function fetchChunkUntil(url: string, byteStart: number, byteEnd: number, timeout: number = 0, maxRetries: number) {
-    return new Promise<any>(async (resolve, reject) => {
-        let retries = 0
-        while (retries < maxRetries) {
-            if (retries > 0) {
-                console.log('! attempt', retries, 'of', maxRetries, `@ ${byteStart}-${byteEnd} ...`)
-            }
+  return new Promise<any>(async (resolve, reject) => {
+    let retries = 0;
+    while (retries < maxRetries) {
+      if (retries > 0) {
+        console.log('! attempt', retries, 'of', maxRetries, `@ ${byteStart}-${byteEnd} ...`);
+      }
 
-            try {
-                const response = await fetchChunk(url, byteStart, byteEnd, timeout)
-                resolve(response)
-                break
-            } catch (e) {}
+      try {
+        const response = await fetchChunk(url, byteStart, byteEnd, timeout);
+        resolve(response);
+        break;
+      } catch (e) { }
 
-            retries++
-            await sleep(100)
-        }
-        reject()
-    })
+      retries++;
+      await sleep(100);
+    }
+    reject();
+  });
 }
 
 async function writeChunks(fileName: string, buffer: Buffer[]) {
-    const fd = await fs.open(fileName, 'w+')
-    for (let k in buffer) {
-        await fd.write(buffer[k])
-    }
-    await fd.close()
+  const fd = await fs.open(fileName, 'w+');
+  for (let k in buffer) {
+    await fd.write(buffer[k]);
+  }
+  await fd.close();
 }
 
 async function getHeaderInfo(url: string) {
-    const info = {
-        connected: false,
-        fileBytes: 0,
-        fileMime: '',
-        supportRanges: false,
-        withHead: false,
-        url
+  const info = {
+    connected: false,
+    fileBytes: 0,
+    fileMime: '',
+    supportRanges: false,
+    withHead: false,
+    url
+  };
+
+  try {
+    console.log('fetching information (1) ... ');
+    let res = await axios.head(url);
+
+    info.connected = true;
+    if (res.headers['content-length']) {
+      info.fileBytes = parseInt(res.headers['content-length']);
     }
-
-    try {
-        console.log('fetching information (1) ... ')
-        let res = await axios.head(url)
-        // console.log(res.headers, '\n')
-
-        info.connected = true
-        if (res.headers['content-length']) {
-            info.fileBytes = parseInt(res.headers['content-length'])
-        }
-        if (res.headers['accept-ranges']) {
-            info.fileMime = res.headers['content-type']
-            info.supportRanges = (res.headers['accept-ranges'] == 'bytes')
-            info.withHead = true
-        }
-    } catch (e) {
-        if (e.response) {
-            console.log('^ server returned code:', e.response.status)
-        } else {
-            info.connected = false
-            console.log('^ failed:', e.message)
-        }
+    if (res.headers['accept-ranges']) {
+      info.fileMime = res.headers['content-type'];
+      info.supportRanges = (res.headers['accept-ranges'] == 'bytes');
+      info.withHead = true;
     }
-
-    try {
-        console.log('fetching information (2) ... ')
-        let res = await axios.get(url, {
-            headers: {
-                range: 'bytes=0-0'
-            }
-        })
-        // console.log(res.headers, '\n')
-
-        info.connected = true
-        info.fileMime = res.headers['content-type']
-        if (res.headers['content-range']) {
-            info.fileBytes = parseInt(res.headers['content-range'].replace('bytes 0-0/', ''))
-            info.supportRanges = (parseInt(res.headers['content-length']) == 1)
-        }
-    } catch (e) {
-        if (e.response) {
-            console.log('^ server returned code:', e.response.status)
-        } else {
-            info.connected = false
-            console.log('^ failed:', e.message)
-        }
+  } catch (e: any) {
+    if (e.response) {
+      console.log('^ server returned code:', e.response.status);
+    } else {
+      info.connected = false;
+      console.log('^ failed:', e.message);
     }
+  }
 
-    return info
+  try {
+    console.log('fetching information (2) ... ');
+    let res = await axios.get(url, {
+      headers: {
+        range: 'bytes=0-0'
+      }
+    });
+
+    info.connected = true;
+    info.fileMime = res.headers['content-type'];
+    if (res.headers['content-range']) {
+      info.fileBytes = parseInt(res.headers['content-range'].replace('bytes 0-0/', ''));
+      info.supportRanges = (parseInt(res.headers['content-length']) == 1);
+    }
+  } catch (e: any) {
+    if (e.response) {
+      console.log('^ server returned code:', e.response.status);
+    } else {
+      info.connected = false;
+      console.log('^ failed:', e.message);
+    }
+  }
+
+  return info;
 }
 
 function downloader(info: any, chunks: number, delay?: number, chunkTimeout?: number, maxRetries?: number) {
-    let resolves = (v: any) => {}
-    let rejects = (v: any) => { }
+  let resolves = (v: any) => { };
+  let rejects = (v: any) => { };
 
-    const emitter = new EventEmitter()
-    const promise = new Promise((resolve, reject) => {
-        resolves = resolve
-        rejects = reject
-    }) as any
-    promise.on = emitter.on
-    promise.emit = emitter.emit
+  const emitter = new EventEmitter();
 
-    //
-    ;(async() => {
-        const bytesPerChunk = Math.floor(info.fileBytes / chunks)
-        const lastChunkOffset = info.fileBytes % chunks
+  const promise = new Promise((resolve, reject) => {
+    resolves = resolve;
+    rejects = reject;
+  }) as any;
 
-        const chunkBuffers = new Array(chunks)
-        const waiting = new Array(chunks)
+  promise.on = emitter.on;
+  promise.emit = emitter.emit;
 
-        for (let i = 0; i < chunks; i++) {
-            let j = (i + 1)
-            const byteOffset = (j == chunks) ? lastChunkOffset : 0
-            const byteStart = bytesPerChunk * i
-            const byteEnd = byteOffset + (bytesPerChunk * j) - 1
+  //
+  (async () => {
+    const bytesPerChunk = Math.floor(info.fileBytes / chunks);
+    const lastChunkOffset = info.fileBytes % chunks;
 
-            if (typeof delay != 'undefined') {
-                await sleep(delay)
-            }
+    const chunkBuffers = new Array(chunks);
+    const waiting = new Array(chunks);
 
-            promise.emit('start', i)
+    for (let i = 0; i < chunks; i++) {
+      let j = (i + 1);
+      const byteOffset = (j == chunks) ? lastChunkOffset : 0;
+      const byteStart = bytesPerChunk * i;
+      const byteEnd = byteOffset + (bytesPerChunk * j) - 1;
 
-            waiting.push(new Promise<void>(async (resolve, reject) => {
-                try {
-                    const { data, headers } = (
-                        (maxRetries) ?
-                            await fetchChunkUntil(info.url, byteStart, byteEnd, chunkTimeout, maxRetries) :
-                            await fetchChunk(info.url, byteStart, byteEnd, chunkTimeout)
-                    )
+      if (typeof delay != 'undefined') {
+        await sleep(delay);
+      }
 
-                    if (headers['content-length'] != (bytesPerChunk + byteOffset)) {
-                        console.log('\n^ chunk size mismatch!, got:', headers['content-length'], '@', `${byteStart}-${byteEnd}`)
-                    }
+      promise.emit('start', i);
 
-                    let buffers: Buffer[] = []
+      waiting.push(new Promise<void>(async (resolve, reject) => {
+        try {
+          const { data, headers } = (
+            (maxRetries) ?
+              await fetchChunkUntil(info.url, byteStart, byteEnd, chunkTimeout, maxRetries) :
+              await fetchChunk(info.url, byteStart, byteEnd, chunkTimeout)
+          );
 
-                    data.on('data', (c: Buffer) => {
-                        buffers.push(c)
-                        promise.emit('data', i, c.length)
-                    })
+          if (headers['content-length'] != (bytesPerChunk + byteOffset)) {
+            console.log('\n^ chunk size mismatch!, got:', headers['content-length'], '@', `${byteStart}-${byteEnd}`);
+          }
 
-                    data.on('end', () => {
-                        chunkBuffers[i] = Buffer.concat(buffers)
-                        promise.emit('end', i, chunkBuffers[i].length)
-                        resolve()
-                    })
+          let buffers: Buffer[] = [];
 
-                    data.on('error', () => {
-                        console.log('\n^ stream error', `@ ${byteStart}-${byteEnd}`)
-                    })
-                } catch (e) {
-                    console.log('\n^ http error', `@ ${byteStart}-${byteEnd}`)
-                }
-            }))
+          data.on('data', (c: Buffer) => {
+            buffers.push(c);
+            promise.emit('data', i, c.length);
+          });
+
+          data.on('end', () => {
+            chunkBuffers[i] = Buffer.concat(buffers);
+            promise.emit('end', i, chunkBuffers[i].length);
+            resolve();
+          });
+
+          data.on('error', () => {
+            console.log('\n^ stream error', `@ ${byteStart}-${byteEnd}`);
+          });
+        } catch (e) {
+          console.log('\n^ http error', `@ ${byteStart}-${byteEnd}`);
         }
-        await Promise.all(waiting)
-        resolves(chunkBuffers)
-    })()
-    //
+      }));
+    }
 
-    return promise
+    await Promise.all(waiting);
+    resolves(chunkBuffers);
+  })()
+  //
+
+  return promise;
 }
 
 async function start(url: string, chunks: number, destFile?: string | boolean, delay?: number, chunkTimeout?: number, maxRetries?: number) {
-    const multibar = new cliProgress.MultiBar({
-        format: ' <{chunk}> {bar} | {value}/{total} ({percentage}%, eta={eta}s)',
-        clearOnComplete: false,
-        hideCursor: false
-    }, cliProgress.Presets.legacy)
+  const multibar = new cliProgress.MultiBar({
+    format: ' <{chunk}> {bar} | {value}/{total} ({percentage}%, eta={eta}s)',
+    clearOnComplete: false,
+    hideCursor: false
+  }, cliProgress.Presets.legacy);
 
-    console.time('time elapsed')
+  console.time('time elapsed');
 
-    const info = await getHeaderInfo(url)
-    if (!info.connected) {
-        console.log('')
-        return;
-    }
+  const info = await getHeaderInfo(url);
+  if (!info.connected) {
+    console.log('');
+    return;
+  }
 
-    if (!info.supportRanges && chunks > 1) {
-        console.log('^ server is not support ranges (use -c 1 instead)\n')
-        return;
-    }
+  if (!info.supportRanges && chunks > 1) {
+    console.log('^ server is not support ranges (use -c 1 instead)\n');
+    return;
+  }
 
-    console.log('\nfile mime:', info.fileMime)
-    console.log('bytes:', info.fileBytes, ' ranges:', info.supportRanges, ' head:', info.withHead)
+  console.log('\nfile mime:', info.fileMime);
+  console.log('bytes:', info.fileBytes, ' ranges:', info.supportRanges, ' head:', info.withHead);
 
-    const chunkBar = multibar.create(chunks, 0, { chunk: '###' })
-    const totalBar = multibar.create(info.fileBytes, 0, { chunk: '###' })
+  const chunkBar = multibar.create(chunks, 0, { chunk: '###' });
+  const totalBar = multibar.create(info.fileBytes, 0, { chunk: '###' });
 
-    const chunkBuffers = await downloader(info, chunks, delay, chunkTimeout, maxRetries)
-        .on('data', (chunkNo: number, received: number) => totalBar.increment(received))
-        .on('end', () => chunkBar.increment(1))
+  const chunkBuffers = await downloader(info, chunks, delay, chunkTimeout, maxRetries)
+    .on('data', (chunkNo: number, received: number) => totalBar.increment(received))
+    .on('end', () => chunkBar.increment(1));
 
-    //
-    multibar.stop()
-    if (typeof destFile != 'string') {
-        let fUrl = new URL(url)
-        let fBasename = path.basename(fUrl.pathname)
-        if (fBasename.length > 0) {
-            destFile = decodeURIComponent(fBasename)
-        } else {
-            let ext = mime.extension(info.fileMime)
-            destFile = `dl-${fUrl.hostname.split('.').join('_')}-${Math.floor(new Date().getTime() / 1000)}.${ext || '.bin'}`
-        }
-    }
-
-    if (info.fileBytes > 2147483647) {
-        console.log('\nwriting chunks into file:', destFile, '...')
-        await writeChunks(destFile, chunkBuffers)
+  //
+  multibar.stop();
+  if (typeof destFile != 'string') {
+    let fUrl = new URL(url);
+    let fBasename = path.basename(fUrl.pathname);
+    if (fBasename.length > 0) {
+      destFile = decodeURIComponent(fBasename);
     } else {
-        console.log('\nwriting to file:', destFile, '...')
-        const fileBuffer = Buffer.concat(chunkBuffers, info.fileBytes)
-        await fs.writeFile(destFile, fileBuffer)
+      let ext = mime.extension(info.fileMime);
+      destFile = `dl-${fUrl.hostname.split('.').join('_')}-${Math.floor(new Date().getTime() / 1000)}.${ext || '.bin'}`;
     }
+  }
 
-    console.log('')
-    console.timeEnd('time elapsed')
-    console.log('')
-    //
+  if (info.fileBytes > 2147483647) {
+    console.log('\nwriting chunks into file:', destFile, '...');
+    await writeChunks(destFile, chunkBuffers);
+  } else {
+    console.log('\nwriting to file:', destFile, '...');
+    const fileBuffer = Buffer.concat(chunkBuffers, info.fileBytes);
+    await fs.writeFile(destFile, fileBuffer);
+  }
+
+  console.log('');
+  console.timeEnd('time elapsed');
+  console.log('');
+  //
 }
 
 export {
-    start, downloader,
-    setUserAgent,
-    getHeaderInfo, writeChunks, fetchChunk
-}
+  start, downloader,
+  setUserAgent,
+  getHeaderInfo, writeChunks, fetchChunk
+};
